06f386745fffa6cfdfbef31ddb988d77675dd17a
Edgar A. Ventura Jorge <eventura@viva.com.do>
2019-10-09 09:04:04 -0400

    Creating README

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let keyword;
+
+class SearchBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+        this.state = { repositories: [] };
+    }
+
+
+    render() {
+        return(
+            <div className="container">
+                <div>
+                  <form>
+                  <input type="text" className="searchBox"  ref={(input) => { this.searchBox = input; }}/>
+                  <button onClick={this.onClick}>Go!</button>
+                  </form>
+                  <div className="foundRepo">{this.props.name}</div>
+                  <div>Enter a keyword</div>
+                </div>
+                <div>
+                  <h2>Repositorios</h2>
+                  <ul>
+                  { this.state.repositories.map( ( item, index ) => (
+                      <li key={ index}>
+                          { item.name }
+                      </li>
+                  )) }
+                  </ul>
+                </div>
+            </div>
+            );
+    }
+
+    onClick(e) {
+
+        keyword = this.searchBox.value;
+        let api = 'https://api.github.com/search/repositories?sort=stars&order=desc&q=' + keyword;
+        console.log(keyword);
+        fetch(api)
+            .then(blob => blob.json())
+            .then(response => {
+                this.setState({ repositories: response.items });
+            });
+        e.preventDefault();
+
+    }
 }
 
-export default App;
+export default SearchBox;
